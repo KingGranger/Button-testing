f5b9e94551afe1c37e8b429e29fff5f180087486
KingGranger <cheeridon@gmail.com>
2018-06-03 22:17:35 -0400

    adding redux

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import Home from './components/Home';
 import ClickedPage from './components/ClickedPage';
 import api from './AuthAdapter/api'
 
-const BaseUrl = 'http://localhost:3000'
-
-const headers = {
-  'Content-Type': 'application/json',
-  Accepts: 'application/json'
-}
-
-const enterContestant = (first_name, last_name, phone_number, terms_of_agreement) => {
-
-  const newContestant = {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify({first_name, last_name, phone_number, terms_of_agreement})
-  }
-  return fetch(`${BaseUrl}/users`, newContestant).then(res => res.json())
-}
 
 class App extends Component {
   state = {
@@ -32,7 +16,7 @@ class App extends Component {
     //   debugger
     //   this.setState({buttonHasBeenClicked: !this.state.buttonHasBeenClicked, contestantId: data.id})
     // })
-    enterContestant(firstName, lastName, phoneNumber, termsOfAgreement).then(data=> {debugger})
+    //enterContestant(firstName, lastName, phoneNumber, termsOfAgreement).then(data=> {debugger})
     //this.setState({buttonHasBeenClicked: !this.state.buttonHasBeenClicked})
   }
   render() {
